e22540258518a02b1ec81d9a07f0c51736c3a829
Riya Verma <rv9858196@gmail.com>
2024-11-15 23:07:09 +0530

    Code change- 2

--- src/XPagination.js
+++ src/XPagination.js
@@ -24,6 +24,7 @@ function XPagination() {
                 setData(result) ;
             }catch(error) {
                 console.error("Error " , error) ;
+                alert("Failed to load data. Please try again later.");
             }
         } ;
         fetchData() ;
